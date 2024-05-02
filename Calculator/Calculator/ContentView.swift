f8a183d4129c5cf59355388d8a77381653d912a9
soaringwave <82134672+treesofgroo@users.noreply.github.com>
2024-05-02 15:55:09 +0900

    Make all buttons

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -13,14 +13,53 @@ struct ContentView: View {
             HStack {
                 Text("0")
                     .font(.largeTitle)
+                    .foregroundStyle(.white)
             }
-            VStack {
-                HStack {
+            VStack(spacing: 10) {
+                HStack(spacing: 10) {
                     CustomButton(text: "AC", image: nil, isOrange: false, isGray: true)
                     CustomButton(text: nil, image: "plus.forwardslash.minus", isOrange: false, isGray: true)
                     CustomButton(text: nil, image: "percent", isOrange: false, isGray: true)
                     CustomButton(text: nil, image: "divide", isOrange: true, isGray: false)
                 }
+                
+                    HStack(spacing: 10) {
+                        CustomButton(text: "7", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "8", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "9", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: nil, image: "multiply", isOrange: true, isGray: false)
+                    }
+                
+                    HStack(spacing: 10) {
+                        CustomButton(text: "4", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "5", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "6", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: nil, image: "minus", isOrange: true, isGray: false)
+                    }
+                
+                    HStack(spacing: 10) {
+                        CustomButton(text: "1", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "2", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: "3", image: nil, isOrange: false, isGray: false)
+                        CustomButton(text: nil, image: "plus", isOrange: true, isGray: false)
+                    }
+                HStack(spacing: 10) {
+                    Button(action: {
+                        print("0 clicked")
+                    }, label: {
+                        Text("0")
+                    })
+                    .font(.largeTitle)
+                    .frame(width: 150, height: 60)
+                    .padding(10)
+                    .foregroundColor(.white)
+                    .background(.black)
+                    .clipShape(.capsule)
+                    CustomButton(text: ".", image: nil, isOrange: false, isGray: false)
+                    CustomButton(text: nil, image: "equal", isOrange: true, isGray: false)
+                }
+
+
             }
         }
         .padding()
